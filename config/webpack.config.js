bafd2073a172be7acd8cbbb7f5ba42db769e8c25
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-02-07 13:41:37 +0100

    feat: add config of devServer

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
     filename: "js/[name].js",
     path: path.resolve(__dirname, "../", "dist"),
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "../", "public"),
+  },
   module: {
     rules: [
       {
