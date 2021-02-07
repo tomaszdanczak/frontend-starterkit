8e115c65e1803f3f5a86baa51480e56a0f33b8ac
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-02-07 12:04:14 +0100

    feat: configurate html-webpack-plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -10,5 +11,12 @@ module.exports = {
     filename: "js/[name].js",
     path: path.resolve(__dirname, "../", "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Strona główna",
+      template: "src/pages/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
