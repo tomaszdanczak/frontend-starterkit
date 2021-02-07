6904ce39bf4bc2b898a190ae8c8976e2245158c4
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-02-07 13:18:13 +0100

    feat: add the function that will use the image

--- src/tools/addImage.js
+++ src/tools/addImage.js
@@ -1,1 +1,7 @@
 import photo from "../images/webpack.jpg";
+
+export default (tag) => {
+  const img = document.createElement("img");
+  img.src = photo;
+  document.querySelector(tag).appendChild(img);
+};
