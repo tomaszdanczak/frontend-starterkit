da549d0c4d5ca8cf2e6f3f0a7ec8c6a1ce06797c
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-02-07 11:47:27 +0100

    feat: add output

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
+
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "../", "dist"),
   },
 };
