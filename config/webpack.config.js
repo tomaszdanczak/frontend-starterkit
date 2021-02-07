47abd2df0bf8faf63595985bccc156518c0280a6
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-02-07 14:02:24 +0100

    feat: add webpack.config.pages.js in webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.pages");
 
-module.exports = {
+module.exports = merge(common, {
   devtool: "source-map",
   mode: "development",
   output: {
@@ -39,4 +40,4 @@ module.exports = {
     ],
   },
   plugins: [new CleanWebpackPlugin()],
-};
+});
